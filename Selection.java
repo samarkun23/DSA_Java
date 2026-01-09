50623693d2a57976f2cf1e22463fde33a1e6a087
samarkun <samarkun4@gmail.com>
2026-01-09 01:02:24 -0500

    insersation short

--- Selection.java
+++ Selection.java
@@ -3,10 +3,22 @@ import java.util.Arrays;
 public class Selection {
 
     public static void main(String[] args) {
-        System.out.println("This is selection short");
+        System.out.println("This is inserstion short");
         int[] arr = {3, 1, 5, 4, 2};
-        SelectionShort(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     static void SelectionShort(int[] arr) {
